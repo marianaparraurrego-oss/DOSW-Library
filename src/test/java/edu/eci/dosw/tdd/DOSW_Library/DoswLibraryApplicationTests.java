1f446e42555e5537ddc9af6817fc9c2163a7593b
Mariana Parra <andrea.parra-u@mail.escuelaing.edu.co>
2026-03-10 22:18:30 -0500

    Agregue estructura

---
+++ src/test/java/edu/eci/dosw/tdd/DOSW_Library/DoswLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.eci.dosw.tdd.DOSW_Library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoswLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
